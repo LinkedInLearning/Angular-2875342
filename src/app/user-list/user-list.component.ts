71a5b2c8b88c4fdebefcf8e2b0d1e91c432a09d1
dperuo <derek@derekperuo.net>
2021-02-24 21:36:49 -0600

    Filter user list using the web storage service

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -13,15 +13,18 @@ export class UserListComponent implements OnInit {
   public users: User[] | null = null;
 
   constructor(
-    private userListService: UserListService
+    private userListService: UserListService,
+    private webStorageService: WebStorageService
   ) { }
 
   public async ngOnInit(): Promise<void> {
-    this.users = await this.userListService.getAll();
+    const filtered = this.webStorageService.get('USERS');
+    this.users = (filtered === null) ? await this.userListService.getAll() : JSON.parse(filtered);
   }
 
   public async update(text: string): Promise<void> {
     this.users = await this.userListService.filter(text);
+    this.webStorageService.set('USERS', JSON.stringify(this.users));
   }
 
 }
