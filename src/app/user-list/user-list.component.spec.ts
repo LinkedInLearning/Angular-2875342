732ca65213f95b584de70a7e86e6587f4369c32c
dperuo <derek@derekperuo.net>
2021-02-28 15:14:36 -0600

    Chapter Challenge: Custom TestBed wrapper

--- src/app/user-list/user-list.component.spec.ts
+++ src/app/user-list/user-list.component.spec.ts
@@ -3,6 +3,16 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { UserListComponent } from './user-list.component';
 import { WebStorageService } from '../services/web-storage.service';
 
+/**
+ * CHALLENGE
+ * 
+ * Make a custom wrapper for Angular's TestBed class
+ * to inject common dependencies into all your specs.
+ * 
+ * This new class should be a drop-in replacement
+ * for Angular's TestBed class.
+ */
+
 describe('UserListComponent', () => {
   let component: UserListComponent;
   let fixture: ComponentFixture<UserListComponent>;
