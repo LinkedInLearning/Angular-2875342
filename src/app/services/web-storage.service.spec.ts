ef3aef1ab7fd874e5ea889cdae258d8f0aa8bf9b
dperuo <derek@derekperuo.net>
2021-02-28 11:25:32 -0600

    Fix dependency injection errors in spec files

--- src/app/services/web-storage.service.spec.ts
+++ src/app/services/web-storage.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClient } from '@angular/common/http';
 
 import { WebStorageService } from './web-storage.service';
 
@@ -6,7 +7,11 @@ describe('WebStorageService', () => {
   let service: WebStorageService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: HttpClient, useValue: jasmine.createSpyObj('HttpClient', [ 'get', 'put' ]) }
+      ]
+    });
     service = TestBed.inject(WebStorageService);
   });
 
--- src/app/user-list/user-list.component.spec.ts
+++ src/app/user-list/user-list.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UserListComponent } from './user-list.component';
+import { WebStorageService } from '../services/web-storage.service';
 
 describe('UserListComponent', () => {
   let component: UserListComponent;
@@ -8,7 +9,10 @@ describe('UserListComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ UserListComponent ]
+      declarations: [ UserListComponent ],
+      providers: [
+        { provide: WebStorageService, useValue: jasmine.createSpyObj('WebStorageService', [ 'getRemove', 'setRemote' ]) }
+      ]
     })
     .compileComponents();
   });
