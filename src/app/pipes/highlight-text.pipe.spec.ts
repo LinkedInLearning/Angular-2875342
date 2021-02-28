e8316852855e37e60e51001dab33b3b8660ea00b
dperuo <derek@derekperuo.net>
2021-02-28 12:29:30 -0600

    Add unit tests for Highlight Text pipe

--- src/app/pipes/highlight-text.pipe.spec.ts
+++ src/app/pipes/highlight-text.pipe.spec.ts
@@ -1,8 +1,25 @@
 import { HighlightTextPipe } from './highlight-text.pipe';
 
 describe('HighlightTextPipe', () => {
+  const pipe = new HighlightTextPipe();
+
   it('create an instance', () => {
-    const pipe = new HighlightTextPipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('wrap a simple string in a <span>', () => {
+    expect(pipe.transform('foobar', 'foo')).toBe('<span class="highlight-text">foo</span>bar')
+  });
+
+  it('wrap an uppercase string in a <span>', () => {
+    expect(pipe.transform('FOOBAR', 'foo')).toBe('<span class="highlight-text">FOO</span>BAR')
+  });
+
+  it('wrap a string with spaces in a <span>', () => {
+    expect(pipe.transform('FOO BAR', 'foo')).toBe('<span class="highlight-text">FOO</span> BAR')
+  });
+
+  it('wrap multiple words in a <span>', () => {
+    expect(pipe.transform('FOO FOO', 'foo')).toBe('<span class="highlight-text">FOO</span> <span class="highlight-text">FOO</span>')
   });
 });
