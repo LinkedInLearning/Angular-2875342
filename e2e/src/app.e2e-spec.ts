5a1a58bc2ab8dc9dbcc25a08a12eae9d6c3590dc
dperuo <derek@derekperuo.net>
2021-03-01 20:33:15 -0600

    Chapter Challenge

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,5 +1,17 @@
 import { AppPage } from './app.po';
 
+/**
+ * CHALLENGE
+ *
+ * Write an e2e test that confirms the user input works as expected.
+ *
+ * When a user searches for 'davis':
+ *
+ *   1. That user should see only two results.
+ *   2. And the word "Davis" should be highlighted
+ *
+ */
+
 describe('User List App', () => {
   const page = new AppPage();
 
